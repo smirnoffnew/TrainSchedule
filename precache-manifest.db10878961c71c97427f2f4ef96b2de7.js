aec24727b8adfe86782b1c2d26334050a4001d9e
smirnoffnew <smirnoffnew@gmail.com>
2019-11-25 17:46:25 +0200

    Updates

---
+++ precache-manifest.db10878961c71c97427f2f4ef96b2de7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f3ef1c94eab0064fa925524d6d9d20",
+    "url": "/TrainSchedule/index.html"
+  },
+  {
+    "revision": "fa3989c18e7cfa241ae5",
+    "url": "/TrainSchedule/static/css/main.6a2d13ca.chunk.css"
+  },
+  {
+    "revision": "385257d0f5a6d0e50fe1",
+    "url": "/TrainSchedule/static/js/2.1089415c.chunk.js"
+  },
+  {
+    "revision": "fa3989c18e7cfa241ae5",
+    "url": "/TrainSchedule/static/js/main.58af5a8d.chunk.js"
+  },
+  {
+    "revision": "c82f21103982a5f0cd0e",
+    "url": "/TrainSchedule/static/js/runtime-main.989b68e1.js"
+  }
+]);
